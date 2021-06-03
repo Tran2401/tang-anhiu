167d82ca35484b2449acabffd404de0de7ad73bd
dopamine03 <85276848+dopamine03@users.noreply.github.com>
2021-06-03 15:01:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 const CONFIG = {
     introTitle: 'Bae oiii!',
     introDesc: `Dạo này có vẻ cục cưng mệt
-cảm giác chán nản ý`,
+cảm giác chán nản ý
+Em muốn ôm cục cưnggg
+Chơm chơm cục cưng nữaaaa`,
     btnIntro: 'Ấn vào đây nèee',
     title: 'Em rất muốn ôm cục cưng
 nhưng chưa làm được điều đó nên em gửi anh cái này nè',
@@ -14,5 +16,5 @@ nhưng chưa làm được điều đó nên em gửi anh cái này nè',
     mess: 'Biết anh yêu cần cái ôm ôm màaa',
     messDesc: 'Hi vọng anh hôm nay sẽ vui hơn hôm qua anh của ngày mai sẽ vui hơn hôm nay.',
     btnAccept: 'Em luôn bên cạnh baee <3',
-    messLink: 'https://www.facebook.com/messages/t/100009011766178 //
+    messLink: 'https://www.facebook.com/messages/t/100009011766178' //
 }
